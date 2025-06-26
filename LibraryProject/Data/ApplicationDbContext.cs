d37a89688a36c5ae83afe9a35827d1345b44ca2e
Minheinkhant-123 <59917179+Minheinkhant-123@users.noreply.github.com>
2025-06-27 01:15:58 +0700

    Add : Add book model and entity framework setup and connection string

---
+++ LibraryProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using LibraryProject.Models;
+using Microsoft.EntityFrameworkCore;
+namespace LibraryProject.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Book> Books => Set<Book>();
+        public DbSet<Author> Authors => Set<Author>();
+        public DbSet<Genre> Genres => Set<Genre>();
+        public DbSet<BookAuthor> BookAuthors => Set<BookAuthor>();
+        public DbSet<BookGenre> BookGenres => Set<BookGenre>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BookAuthor>().HasKey(ba => new { ba.BookId, ba.AuthorId });
+            modelBuilder.Entity<BookGenre>().HasKey(bg => new { bg.BookId, bg.GenreId });
+        }
+    }
+}
--- LibraryProject/Program.cs
+++ LibraryProject/Program.cs
@@ -1,5 +1,11 @@
+using LibraryProject.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Connect to local SQL Server
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
